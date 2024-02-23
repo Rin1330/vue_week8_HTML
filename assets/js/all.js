f74e8ab413dc65bf836e3d3aee58a63e8c89cf25
sakimotorin <sakimotorin@gmail.com>
2024-02-23 12:33:16 +0800

    swiper testing

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,30 +1,37 @@
-import * as bootstrap from 'bootstrap';
-import Swiper from 'swiper/bundle';
+// import * as bootstrap from 'bootstrap';
+// import Swiper from 'swiper/bundle';
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'
 
 // console.log(Swiper);
 
-const menuBtn = document.querySelector('#menuBtn');
-const menuCloseBtn = document.querySelector('#menuCloseBtn');
-const bsOffcanvas = new bootstrap.Offcanvas('#mainMenu');
+// const menuBtn = document.querySelector('#menuBtn');
+// const menuCloseBtn = document.querySelector('#menuCloseBtn');
+// const bsOffcanvas = new bootstrap.Offcanvas('#mainMenu');
 
-menuBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  bsOffcanvas.show();
-})
+// menuBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   bsOffcanvas.show();
+// })
 
-menuCloseBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  bsOffcanvas.hide();
-})
+// menuCloseBtn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   bsOffcanvas.hide();
+// })
 
 const indexFunction = {
   mainBannerInit: () => {
     const swiper = new Swiper('#mainBanner', {
-      // Optional parameters
-      direction: 'horizontal',
       loop: true,
-    
-      // If we need pagination
+      initialSlide: 0,
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+      centeredSlides: false,
+      spaceBetween: 0,
+      watchOverflow: true,
+      // autoplay: {
+      //   delay: 3000,
+      //   pauseOnMouseEnter: true
+      // },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -32,14 +39,28 @@ const indexFunction = {
           return `<span class="${className}">${idx + 1}</span>`
         }
       },
-    
-    
-      // Navigation arrows
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '#mainBanner .swiper-button-next',
+        prevEl: '#mainBanner .swiper-button-prev',
       },
     });
+    // const swiper = new Swiper("#mySwiper", {
+    //   slidesPerView: 4,
+    //   slidesPerGroup: 1,
+    //   centeredSlides: false,
+    //   loop: true,
+    //   slideToClickedSlide: true,
+    //   spaceBetween: 10,
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    //   },
+    //     // Navigation arrows
+    //   navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    //   },
+    // });
   },
   otherInit: () => {
     console.log('other');
